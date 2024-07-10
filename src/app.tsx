2f9c8a80d91936c702f0b7fae063f928c6fe8b69
fernanda.degolin <fernanda.degolin@g.globo>
2024-07-10 14:48:28 -0300

    feat: cria formularios

--- src/app.tsx
+++ src/app.tsx
@@ -2,9 +2,11 @@ import {
   ArrowRight,
   AtSign,
   Calendar,
+  Mail,
   MapPin,
   Plus,
   Settings2,
+  User,
   UserRoundPlus,
   X,
 } from "lucide-react";
@@ -13,12 +15,13 @@ import { FormEvent, useState } from "react";
 export function App() {
   const [isGuestsInputOpen, setIsGuestsInputOpen] = useState(false);
   const [isGuestsModalOpen, setIsGuestsModalOpen] = useState(false);
+  const [isConfirmTripModalOpen, setIsConfirmTripModalOpen] = useState(false);
   const [emailsToInvite, setEmailsToInvite] = useState([
-    "fernandadegolin@hotmail.com",
-    "jessicacrusco@gmail.com",
-    "magalidegolin@gmail.com",
-    "euzebiodegolin@yahoo.com.br",
+    "fernanda.degolin@gmail.com",
   ]);
+
+  const peopleInvited =
+    emailsToInvite.length > 1 ? "pessoas convidadas" : "pessoa convidada";
 
   function openGuestsInput() {
     setIsGuestsInputOpen(true);
@@ -34,6 +37,13 @@ export function App() {
 
   function closeGuestsModal() {
     setIsGuestsModalOpen(false);
+  }
+
+  function openConfirmTripModal() {
+    setIsConfirmTripModalOpen(true);
+  }
+  function closeConfirmTripModal() {
+    setIsConfirmTripModalOpen(false);
   }
 
   function addNewEmailToInvite(event: FormEvent<HTMLFormElement>) {
@@ -120,11 +130,18 @@ export function App() {
                 className="flex items-center gap-2 flex-1 "
               >
                 <UserRoundPlus className="size-5 text-zinc-400" />
-                <span className="text-zinc-400 text-lg flex-1 text-left">
-                  Quem estará na viagem?
-                </span>
+                {emailsToInvite.length > 0 ? (
+                  <span className="text-zinc-100 text-lg flex-1 text-left">
+                    {`${emailsToInvite.length} ${peopleInvited}`}
+                  </span>
+                ) : (
+                  <span className="text-zinc-400 text-lg flex-1 text-left">
+                    Quem estará na viagem?
+                  </span>
+                )}
               </button>
 
+              {/* Modal Guest */}
               {isGuestsModalOpen && (
                 <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
                   <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
@@ -192,10 +209,79 @@ export function App() {
                   </div>
                 </div>
               )}
+              {/* End Modal Guest */}
+
+              {/* Modal Confirm */}
+              {isConfirmTripModalOpen && (
+                <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
+                  <div className="w-[640px] rounded-xl py-5 px-6 shadow-shape bg-zinc-900 space-y-5">
+                    <div className="space-y-2">
+                      <div className="flex items-center justify-between">
+                        <h2 className="font-lg font-semibold">
+                          Confirmar criação da viagem
+                        </h2>
+                        <button>
+                          <X
+                            className="size-5 text-zinc-400"
+                            onClick={closeConfirmTripModal}
+                          />
+                        </button>
+                      </div>
+
+                      <p className="text-sm text-zinc-400 text-left">
+                        Para concluir a criação da viagem para{" "}
+                        <span className="font-semibold text-zinc-100">
+                          Florianópolis, Brasil
+                        </span>{" "}
+                        nas datas de{" "}
+                        <span className="font-semibold text-zinc-100">
+                          16 a 27 de Agosto de 2024
+                        </span>{" "}
+                        preencha seus dados abaixo:
+                      </p>
+                    </div>
+
+                    <form onSubmit={addNewEmailToInvite} className="space-y-3">
+                      <div className="px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2 h-14">
+                        <User className="size-5 text-zinc-400" />
+                        <input
+                          type="text"
+                          name="name"
+                          placeholder="Seu nome completo"
+                          className="placeholder-zinc-400 text-base bg-transparent outline-none flex-1"
+                        />
+                      </div>
+
+                      <div className="px-4 bg-zinc-950 border border-zinc-800 rounded-lg flex items-center gap-2 h-14">
+                        <Mail className="size-5 text-zinc-400" />
+                        <input
+                          type="email"
+                          name="email"
+                          placeholder="Seu e-mail pessoal"
+                          className="placeholder-zinc-400 text-base bg-transparent outline-none flex-1"
+                        />
+                      </div>
+                    </form>
+
+                    <button
+                      type="submit"
+                      onClick={closeConfirmTripModal}
+                      className="text-base text-lime-950 rounded-lg h-11 font-medium bg-lime-300  gap-2 hover:bg-lime-400 w-full"
+                    >
+                      Confirmar criação da viagem
+                    </button>
+                  </div>
+                </div>
+              )}
+
+              {/* End Modal Confirm */}
 
               <div className="w-px h-6 bg-zinc-800" />
 
-              <button className="text-base text-lime-950 rounded-lg px-5 py-2 font-medium bg-lime-300 flex items-center gap-2 hover:bg-lime-400">
+              <button
+                onClick={openConfirmTripModal}
+                className="text-base text-lime-950 rounded-lg px-5 py-2 font-medium bg-lime-300 flex items-center gap-2 hover:bg-lime-400"
+              >
                 Confirmar viagem
                 <ArrowRight className="size-5 text-lime-950" />
               </button>
